a9966673eed68728d52767b3e895cfb3a3fd0b8b
odd:odd <eehd80yul@gmail.com>
2024-11-27 15:17:18 +0900

    swiper 적용

---
+++ src/app/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import IntroSwiper from './intro'
+
+const Home = () => {
+  return (
+    <div>
+      <IntroSwiper />
+    </div>
+  )
+}
+
+export default Home
--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,16 @@ import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import Main from "@/components/layout/Main";
 import { Container } from "@chakra-ui/react";
+import Home from "./home/home";
+import IntroSwiper from "./home/intro";
 
-export default function Home() {
+export default function Page() {
   return (
     <>
       <Header />
       <Main>
-        <Container className="py-6">
-          main
+        <Container>
+          <Home />
         </Container>
       </Main>
       <Footer />
