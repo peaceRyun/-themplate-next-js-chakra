83af1ede2bcbf0fae5f2d841e6d0e335df5dd9c0
odd:odd <eehd80yul@gmail.com>
2024-11-27 13:18:54 +0900

    header 제작

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,16 @@
+"use client";
+
+import { Container, Flex, Heading } from '@chakra-ui/react'
 import React from 'react'
 
 const Header = () => {
   return (
-    <header>header</header>
+    <header>
+      <Container className='flex justify-between items-center'>
+        <Heading as="h1">logo</Heading>
+        <nav>nav</nav>
+      </Container>
+    </header>
   )
 }
 
